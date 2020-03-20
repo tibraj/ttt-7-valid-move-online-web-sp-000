ac047bb3ae4d83b5418108d93ff858eb4a65f346
Taulant Ibraj <tibraj94@gmail.com>
2020-03-20 19:29:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  user_move = board[index].to_i + 1
-  if (board[index] == (0 <= board[index] <= 8)) && position_taken? == false
+  if 0 <= board[index] <= 8 && position_taken? == false
     return true
   end
 end
